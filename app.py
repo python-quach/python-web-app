61282b2dc5de36ee8564b950754c000ba7fc9511
Python Development <python@Hungs-MacBook-Pro.local>
2020-02-14 18:58:45 -0800

    delte cache

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def submit():
         data = Feedback(customer, dealer, rating, comments)
         db.session.add(data)
         db.session.commit()
+        send_email(customer, dealer, rating, comments)
         return render_template('success.html')
     return render_template('index.html', message="You have already submitted feedback")
 
