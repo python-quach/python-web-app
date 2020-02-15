301cd9833ffb32aa3c66ba9718bfce9fccfe3c84
Python Development <python@Hungs-MacBook-Pro.local>
2020-02-14 18:58:03 -0800

    change single quote to double quote

--- send_mail.py
+++ send_mail.py
@@ -14,12 +14,11 @@ def send_email(customer, dealer, rating, comments):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
     login = 'e1c8110fdc14f3'
-    password = '51cbea0b9be8d7',
+    password = "51cbea0b9be8d7"
     message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
 
     sender_email = 'hung.quach@gmail.com'
     reciver_email = 'xenoki@me.com'
-
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'Lexus Feedback'
     msg['From'] = sender_email
